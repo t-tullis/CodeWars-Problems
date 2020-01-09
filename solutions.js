bad90c98c279a3a78adbee9850bb003bb0101433
t-tullis <t.tullis34@gmail.com>
2020-01-08 21:54:41 -0800

    Completed one challenge

---
+++ solutions.js
@@ -0,0 +1,16 @@
+// PROBLEM 1
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// function squareDigits(num){
+//     //may the code be with you
+//      let convertNum = String(num)
+//      let numArr = [];
+//      for(let i = 0; i < convertNum.length; i++){
+//          let convertAndSquare = Number(convertNum[i] * convertNum[i])
+//          numArr.push(convertAndSquare)
+//      }
+//      return Number(numArr.join(''))
+//   }
+
+//   console.log(squareDigits(9119))
