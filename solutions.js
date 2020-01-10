5282a5568b87b4f98b9638d70089a856d46ddeb9
t-tullis <t.tullis34@gmail.com>
2020-01-09 21:57:09 -0800

    Completed a challenge with two different solutions

--- solutions.js
+++ solutions.js
@@ -14,3 +14,26 @@
 //   }
 
 //   console.log(squareDigits(9119))
+
+// PROBLEM 2 "Confirm Ending"
+
+// function solution(str, ending){
+        //Solution 1
+//     // TODO: complete
+//     let string = ''
+//     for(let i = str.length - ending.length; i <= str.length - 1; i++){
+//       string += str[i] 
+//     }
+//     console.log(string)
+//         if(string === ending){
+//             return true 
+//         }else{
+//             return false
+//         }
+//   }
+        
+        // Solution 2
+// // return str.endsWith(ending)
+// // }
+
+//   console.log(solution('abcde', 'cdea'))
