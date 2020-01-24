b4511365c50f5e120c021345e2fd90b65f5f0914
t-tullis <t.tullis34@gmail.com>
2020-01-24 14:21:02 -0800

    Completed algo that rotated an array

--- easy-algos.js
+++ easy-algos.js
@@ -127,3 +127,15 @@ Explanation: 1200 + 34 = 1234
 
 // console.log(addToArrayForm([1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3], 516))
 
+//Problem 10
+//Rotating an array
+// var rotate = function(nums, k) {
+//         for(let i = 0; i < k; i++){
+//                 let poppedVal = nums.pop()
+//                 nums.unshift(poppedVal)
+//         }
+//         return nums
+// };
+
+// console.log(rotate([1,2,3,4,5,6,7], 3))
+
