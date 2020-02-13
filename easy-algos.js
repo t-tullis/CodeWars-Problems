7e4f5c1ccb24f610a6f332a9392b0251120b7cac
t-tullis <t.tullis34@gmail.com>
2020-02-12 22:17:48 -0800

    completed largest of four algo from freecodecamp

--- easy-algos.js
+++ easy-algos.js
@@ -386,3 +386,15 @@ Explanation: 1200 + 34 = 1234
 
 //       console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
 
+//Problem 22
+// function largestOfFour(arr) {
+//         // You can do this!
+//         let newArr = []
+//         for(let i = 0; i < arr.length; i++){
+//                 newArr.push(Math.max(...arr[i]))
+//         }
+//         return newArr
+// }
+      
+// console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
