c941a38767225c37667e5c123f7816d0e59f5c3f
t-tullis <t.tullis34@gmail.com>
2020-02-13 22:18:21 -0800

    Looked at binary search and refactored code

--- easy-algos.js
+++ easy-algos.js
@@ -398,3 +398,37 @@ Explanation: 1200 + 34 = 1234
       
 // console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
+//problem 23
+//Binary Search
+// function binarySearch(arr, elem) {
+//         var start = 0;
+//         var end = arr.length - 1;
+//         var middle = Math.floor((start + end) / 2);
+//         while(arr[middle] !== elem && start <= end) {
+//             if(elem < arr[middle]){
+//                 end = middle - 1;
+//             } else {
+//                 start = middle + 1;
+//             }
+//             middle = Math.floor((start + end) / 2);
+//         }
+//         if(arr[middle] === elem){
+//             return middle;
+//         }
+//         return -1;
+//     }
+    
+//     // Refactored Version
+//     function binarySearch(arr, elem) {
+//         var start = 0;
+//         var end = arr.length - 1;
+//         var middle = Math.floor((start + end) / 2);
+//         while(arr[middle] !== elem && start <= end) {
+//             if(elem < arr[middle]) end = middle - 1;
+//             else start = middle + 1;
+//             middle = Math.floor((start + end) / 2);
+//         }
+//         return arr[middle] === elem ? middle : -1;
+//     }
+    
+//     binarySearch([2,5,6,9,13,15,28,30], 103)
