621baaf10b856c7c3e75ac61c17017908ab1e52f
t-tullis <t.tullis34@gmail.com>
2020-02-18 15:41:16 -0800

    started implementing sorting algos

--- easy-algos.js
+++ easy-algos.js
@@ -389,3 +389,6 @@ Explanation: 1200 + 34 = 1234
 // console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
 
+var findShortestSubArray = function(nums) {
+        return nums.length - 1
+};
