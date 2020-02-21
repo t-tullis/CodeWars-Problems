df5f36266116289930d2e7c3a7ca3844a42c1f87
t-tullis <t.tullis34@gmail.com>
2020-02-20 19:02:20 -0800

    implemented selection sort

--- sorting-algos.js
+++ sorting-algos.js
@@ -14,3 +14,21 @@
 // }
 
 // console.log(bubbleSort([27, 48, 1, 9, 33]))
+
+//SELECTION SORT
+// function selectionSort(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         let lowest = i
+//         for(let j = i + 1; j < arr.length; j++){
+//             if(arr[j] < arr[lowest]){
+//                 lowest = j;
+//             }
+//         }
+//         let temp = arr[i]
+//         arr[i] = arr[lowest]
+//         arr[lowest] = temp
+//     }
+//     return arr
+// }
+
+// console.log(selectionSort([6,2,8,9,3,1]))
